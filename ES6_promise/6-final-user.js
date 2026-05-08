eaca9af62c32866c53f548d9ee38f0a42d2e4ab0
DenZzz1 <m2nky2fficiel@gmail.com>
2026-05-08 12:08:44 +0000

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,4 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
-
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) =>
-    results.map((result) => ({
-      status: result.status,
-      value: result.value || result.reason,
-    }))
-  );
-}
+results.map((result) => ({
+  status: result.status,
+  value: result.value || result.reason.toString(),
+}))
