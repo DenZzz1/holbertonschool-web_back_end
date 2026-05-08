89a8db3755ba1a48525e5a727c514c103af84d9b
DenZzz1 <m2nky2fficiel@gmail.com>
2026-05-08 12:10:02 +0000

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,4 +1,14 @@
-results.map((result) => ({
-  status: result.status,
-  value: result.value || result.reason.toString(),
-}))
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value: result.value || result.reason.toString(),
+    }))
+  );
+}
